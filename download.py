3034f5bb5c88e5bcb8b407e4064ee03b62a8b477
AbyssLink <26038696+AbyssLink@users.noreply.github.com>
2020-01-16 12:23:26 +0800

    fix: file name too long exception

--- download.py
+++ download.py
@@ -39,6 +39,13 @@ default_path: str = os.path.join(
 
 
 def main_download(tags_: str, start_: int, end_: int, path_: str):
+    """
+
+    :param tags_:
+    :param start_:
+    :param end_:
+    :param path_:
+    """
     if not os.path.exists(path_):
         logger.warning(f"Download Path='{path_}' does not exist, use default path={default_path}\n")
         path_ = default_path
@@ -46,6 +53,8 @@ def main_download(tags_: str, start_: int, end_: int, path_: str):
             os.makedirs("download")
     try:
         now = datetime.now()
+        count = 1
+        file_size = 0
         for i in range(start_, end_ + 1):
             yande = Yande(tags_=tags_, page_=i)
             logger.info(f"Request API URL = {yande.url()}")
@@ -54,8 +63,7 @@ def main_download(tags_: str, start_: int, end_: int, path_: str):
             if status == 200:
                 posts = json.loads(r.content)['posts']
                 logger.info(f"Page# {i} : {str(len(posts))} images will be downloaded...\n")
-                count = 1
-                file_size = 0
+
                 for post_info in posts:
                     logger.info(f"Img# {count}")
                     download_pic(url_=post_info['file_url'], id_=post_info['id'], tags_=post_info['tags'],
@@ -75,10 +83,18 @@ def main_download(tags_: str, start_: int, end_: int, path_: str):
 
 
 def download_pic(url_: str, path_: str, tags_: str, id_: str, size_: float):
+    """
+
+    :param url_:
+    :param path_:
+    :param tags_:
+    :param id_:
+    :param size_:
+    """
     sleep_time = round(random.random() * 10, 2)
     logger.info(f"Timed sleep for {str(sleep_time)}s")
     time.sleep(sleep_time)
-    logger.info(f"Start Downloading id = {id_} Size = {str(round(size_ / 1024 / 1024, 2))}MiB, please wait.")
+    logger.info(f"Start downloading id = {id_} Size = {str(round(size_ / 1024 / 1024, 2))}MiB, please wait.")
 
     r = requests.get(url=url_, headers=headers, stream=True)
     status = r.status_code
@@ -90,19 +106,22 @@ def download_pic(url_: str, path_: str, tags_: str, id_: str, size_: float):
         t = tqdm(total=total_size, unit='iB', unit_scale=True)
 
         file_extension = os.path.splitext(urlparse(url_).path)[1]
-        base_name = f"yande.re {id_} {tags_.replace('/', '_')}{file_extension}"
+        base_name = f"yande.re {id_} {optimize_tags(tags_=tags_)}{file_extension}"
         file_path = os.path.join(path_, base_name)
 
-        with open(file_path, 'wb') as f:
-            for data in r.iter_content(block_size):
-                t.update(len(data))
-                f.write(data)
+        try:
+            with open(file_path, 'wb') as f:
+                for data in r.iter_content(block_size):
+                    t.update(len(data))
+                    f.write(data)
+        except Exception as e:
+            logger.error(e)
 
         t.close()
         if total_size != 0 and t.n != total_size:
             logger.error("ERROR, something went wrong")
 
-        logger.info(f"Download Complete, Save path {file_path}\n")
+        logger.info(f"Download successfully.\n")
     else:
         logger.error(f"HTTP_STATUS: {status}. file in {url_} failed to download. "
                      f"Retry in 1 sec. {url_}")
@@ -111,6 +130,41 @@ def download_pic(url_: str, path_: str, tags_: str, id_: str, size_: float):
 
 def save_info(info_: dict, path_: str):
     path_ = os.path.join(path_, str(datetime.now().strftime(f"%m-%d-%Y %H_%M_%S")))
+
+
+def optimize_tags(tags_: str):
+    """
+    optimize the tags string, prevent path errors and filenames from being too long
+    :param tags_:
+    """
+    tags_ = tags_.replace('/', '_')
+    tags_ = tags_.replace(':', '_')
+    # Limit file name length for 120 character
+    if len(tags_) <= 120:
+        return tags_
+    else:
+        tag_list = tags_.split(' ')
+        new_tags = ''
+        index = 0
+        while len(new_tags) < 120:
+            if new_tags != '':
+                new_tags = new_tags + ' ' + tag_list[index]
+            else:
+                new_tags = new_tags + tag_list[index]
+            index = index + 1
+        return new_tags
+
+
+def test():
+    download_pic(
+        url_='https://files.yande.re/image/57cb51d1f8e85cd99c34af016b687d40/yande.re%20601083%20ass%20breast_hold'
+             '%20doi_tamako%20feet%20iyojima_anzu%20koori_chikage%20masuzu_aki%20megane%20naked%20nipples'
+             '%20nogi_wakaba%20pubic_hair%20pussy%20uesato_hinata%20uncensored.png', id_='601083',
+        tags_='aces8492unsung akihara_sekka ass breast_hold doi_tamako feet fujimori_mito hanamoto_yoshika '
+              'iyojima_anzu kohagura_natsume koori_chikage masuzu_aki megane naked nipples nogi_wakaba '
+              'nogi_wakaba_wa_yuusha_de_aru pubic_hair pussy shiratori_utano takashima_yuuna uesato_hinata uncensored '
+              'yuuki_yuuna_wa_yuusha_de_aru yuuki_yuuna_wa_yuusha_de_aru:_hanayui_no_kirameki',
+        size_=3097256, path_=default_path)
 
 
 if __name__ == "__main__":
